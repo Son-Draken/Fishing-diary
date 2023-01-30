dd332d542cc95bcf9a3e42b038466fc0db0e1167
Son-Draken <Kovis12@seznam.cz>
2023-01-30 15:29:47 +0100

    zaklad Ui

---
+++ DiarRyby/StatistikaPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiarRyby
+{
+    /// <summary>
+    /// Interakční logika pro StatistikaPage.xaml
+    /// </summary>
+    public partial class StatistikaPage : Page
+    {
+        public StatistikaPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
